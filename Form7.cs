cffc667ea43bad8a2336eff5b708fd7247918b1e
mateu <mateusz.kwiatkowski@wws-insurance.pl>
2023-12-04 13:11:14 +0100

    Datebase connector

--- Form7.cs
+++ Form7.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Firebase.Database;
+using Firebase.Database.Query;
 
 namespace WWS_Trimmer
 {
     public partial class Form7 : Form
     {
+        private const string FirebaseUrl = "https://quick-pdf-auth-default-rtdb.europe-west1.firebasedatabase.app/";
+        private const string ActivationCodeNode = "activationCode";
+        private readonly FirebaseClient firebaseClient = new FirebaseClient(FirebaseUrl);
+        private const string ExpectedActivationCode = "512325468";
         public Form7()
         {
             InitializeComponent();
@@ -20,13 +26,54 @@ namespace WWS_Trimmer
 
         }
 
-        private void Timer1_Tick(object sender, EventArgs e)
+        private void ShowExitMessageBox()
+        {
+            
+            DialogResult result = MessageBox.Show(
+                "Dostęp do aplikacji został zablokowany, skontaktuj się z producentem",
+                "Blokada dostępu",
+                MessageBoxButtons.OK, 
+                MessageBoxIcon.Error 
+            );
+
+            // Sprawdzenie, czy użytkownik nacisnął OK
+            if (result == DialogResult.OK)
+            {
+                // Zamknięcie całej aplikacji
+                Application.Exit();
+            }
+        }
+
+        // Uwaga! Kod odpowiadajacy za weryfikację aktualizacji z bazą danych WWS Insurance jest ukryty na potrzeby opubblikowania pozostałego kodu źródłowego w GitHub
+        private async void Timer1_Tick(object sender, EventArgs e)
         {
             timer1.Stop();
-            Hide();
-            Form1 mainForm = new Form1();
-            mainForm.ShowDialog();
-            Close();
+
+            // Sprawdzanie kodu w Firebase
+            string firebaseActivationCode = await GetFirebaseActivationCode();
+
+            if (string.Equals(firebaseActivationCode, ExpectedActivationCode, StringComparison.OrdinalIgnoreCase))
+            {
+                Hide();
+                Form1 mainForm = new Form1();
+                mainForm.ShowDialog();
+                Close();
+            }
+            else
+            {
+                ShowExitMessageBox();
+            }
+
+            
+        }
+
+        private async Task<string> GetFirebaseActivationCode()
+        {
+            var activationCodeSnapshot = await firebaseClient
+                .Child(ActivationCodeNode)
+                .OnceSingleAsync<string>();
+
+            return activationCodeSnapshot ?? string.Empty;
         }
         private void button2_Click(object sender, EventArgs e)
         {
